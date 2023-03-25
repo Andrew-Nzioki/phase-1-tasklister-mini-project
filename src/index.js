849009634adfb785bd22c962079b2c32ba274026
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-25 14:59:46 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',()=>{
+  let form=document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+      e.preventDefault()
+      buildTask(e.target.newtask.value)
+      form.reset()
+  })
+})
+
+function buildTask(task){
+  let li=document.createElement('li')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent=' x'
+  li.textContent=`${task} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  console.log(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
